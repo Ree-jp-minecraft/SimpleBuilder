00fb40a1e0e272b42ab591285052ec288180b4ff
Ree_ <ree.dev.jp@gmail.com>
2020-06-04 22:36:07 +0900

    update: copyright

--- src/main/kotlin/net/ree_jp/simplebuilder/SimpleBuilderPlugin.kt
+++ src/main/kotlin/net/ree_jp/simplebuilder/SimpleBuilderPlugin.kt
@@ -1,14 +1,12 @@
 /*
+ * RRRRRR                         jjj
+ * RR   RR   eee    eee               pp pp
+ * RRRRRR  ee   e ee   e _____    jjj ppp  pp
+ * RR  RR  eeeee  eeeee           jjj pppppp
+ * RR   RR  eeeee  eeeee          jjj pp
+ *                              jjjj  pp
  *
- *  * RRRRRR                         jjj
- *  * RR   RR   eee    eee               pp pp
- *  * RRRRRR  ee   e ee   e _____    jjj ppp  pp
- *  * RR  RR  eeeee  eeeee           jjj pppppp
- *  * RR   RR  eeeee  eeeee          jjj pp
- *  *                              jjjj  pp
- *  *
- *  * Copyright (c) 2020. Ree-jp.  All Rights Reserved.
- *
+ * Copyright (c) 2020. Ree-jp.  All Rights Reserved.
  */
 
 package net.ree_jp.simplebuilder
